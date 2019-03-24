93fce960ab9169a0411531215b8bc14e8581b291
Arslan Ali <ali.arslan319@gmail.com>
2019-03-24 02:34:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,3 +10,9 @@ def first_element(my_first_element)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_first_element = taylor_swift[0]
 end
+
+def third_element(my_third_element)
+    taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    my_third_element = taylor_swift[2]
+end
+  
