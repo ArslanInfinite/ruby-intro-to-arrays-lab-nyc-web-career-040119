a3e3c3b1bdfd7ceb9c4e5f391f73f64b38de5c5f
Arslan Ali <ali.arslan319@gmail.com>
2019-03-24 02:42:55 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,38 @@
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements
+  array = [1, 2]
+end
+
+def first_element(my_first_element)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = taylor_swift[0]
+end
+
+def third_element(my_third_element)
+    taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    my_third_element = taylor_swift[2]
+end
+
+def last_element(my_last_element)
+    taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    my_last_element = taylor_swift[3]
+end
+
+def first_element_with_array_methods(first_country)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  first_country = south_east_asia[0]
+end
+
+def last_element_with_array_methods(last_country)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  last_country = south_east_asia[3]
+end
+
+def length_of_array(length_of_array)
+  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  length_of_array = programming_languages.size
+end
+  
