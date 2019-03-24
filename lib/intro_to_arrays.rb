1f6ea5cf1abb7d3b49f891c91815c238882a30f2
Arslan Ali <ali.arslan319@gmail.com>
2019-03-24 02:28:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,7 @@ end
 def array_with_two_elements
   array = [1, 2]
 end
+
+def first_element
+  my_first_element = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element.unshift "taylor_swift"
